84c49d3582291cd550ed1c5884d3471cd0eebd75
Debjyoti Sarkar <dj.iitian@gmail.com>
2021-08-20 23:10:15 +0800

    test cases for GC storage policy

---
+++ src/test/java/com/sc/caching/StandardCacheNullGCStressTest.java
@@ -0,0 +1,44 @@
+package com.sc.caching;
+
+import com.sc.caching.policies.StoragePolicy;
+import edu.umd.cs.mtc.MultithreadedTestCase;
+import edu.umd.cs.mtc.TestFramework;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.mockito.Mockito.*;
+
+public class StandardCacheNullGCStressTest extends MultithreadedTestCase {
+    private Cache<String, String> cache;
+    private Function<String, String> mapping;
+
+    @Override
+    public void initialize() {
+        mapping = mock(Function.class);
+        when(mapping.apply(eq("DJ"))).thenReturn(null);
+        cache = new StandardCache<>(mapping, StoragePolicy.GC_OPTMIZED);
+    }
+
+    public void thread1() throws InterruptedException {
+        cache.get("DJ");
+    }
+
+    public void thread2() throws InterruptedException {
+        cache.get("DJ");
+    }
+
+    public void thread3() throws InterruptedException {
+        cache.get("DJ");
+    }
+
+    @Override
+    public void finish() {
+        verify(mapping, times(1)).apply(eq("DJ"));
+    }
+
+    @Test
+    public void testCounter() throws Throwable {
+        TestFramework.runManyTimes(new StandardCacheNullGCStressTest(), 100);
+    }
+}
